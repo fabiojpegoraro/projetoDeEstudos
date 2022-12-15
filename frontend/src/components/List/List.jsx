65dfbef9699f553434b9629d3a730843f180673f
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-12-14 22:27:58 -0300

    Evolução do layout da lista de clientes

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -3,27 +3,32 @@ import './style.css';
 
 const List = () => {
   return (
-    <table className="table">
-      <thead className="tableHead">
-        <tr>
-          <th>Código</th>
-          <th>Nome</th>
-          <th>Idade</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody className="tableBody">
-        <tr>
-          <td>123</td>
-          <td>Fábio</td>
-          <td>36</td>
-          <td>
-            <button>Edit</button>
-            <button>Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <div className="newClientDivButton">
+        <button className="newClientButton">Novo Cliente</button>
+      </div>
+      <table className="table">
+        <thead className="tableHead">
+          <tr>
+            <th className="firtsCell">Código</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th className="secondCell">Ações</th>
+          </tr>
+        </thead>
+        <tbody className="tableBody">
+          <tr>
+            <td className="thirdCell">123</td>
+            <td>Fábio</td>
+            <td>36</td>
+            <td className="fourthCell">
+              <button>Edit</button>
+              <button>Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 };
 
