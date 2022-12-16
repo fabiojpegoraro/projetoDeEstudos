698491089c17d9be8ce869a29f904d56bc35a3f3
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-12-15 23:03:05 -0300

    fetch de clientes

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './style.css';
 
 const List = () => {
+  const [clients, setClients] = useState([]);
+
+  const getClients = () => {
+    fetch('http://localhost:3000/clients')
+      .then((response) => response.json())
+      .then((json) => setClients(json));
+  };
+
+  useEffect(() => {
+    getClients();
+  }, []);
+
   return (
     <>
       <div className="newClientDivButton">
@@ -17,15 +31,23 @@ const List = () => {
           </tr>
         </thead>
         <tbody className="tableBody">
-          <tr>
-            <td className="thirdCell">123</td>
-            <td>Fábio</td>
-            <td>36</td>
-            <td className="fourthCell">
-              <button>Edit</button>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {clients.map((client, index, clients) => {
+            return (
+              <tr>
+                <td className={clients.length - 1 === index ? 'thirdCell' : ''}>
+                  {client.id}
+                </td>
+                <td>{client.name}</td>
+                <td>{client.idade}</td>
+                <td
+                  className={clients.length - 1 === index ? 'fourthCell' : ''}
+                >
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
