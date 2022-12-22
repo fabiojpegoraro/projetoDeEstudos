35a5fda1ebd9e086ce559f1c7ad5d631e8eb0d80
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-12-22 17:21:46 -0300

    Adição de estados e implementação do post do formulário

--- frontend/src/components/ClientForm/ClientForm.jsx
+++ frontend/src/components/ClientForm/ClientForm.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../Layout/Layout';
 import './style.css';
 
 const ClientForm = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  const saveClient = () => {
+    fetch('http://localhost:3000/clients', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name,
+        idade: age,
+      }),
+    }).then((response) => {
+      if (response.status === 201) {
+        window.alert('Cliente incluído com sucesso!');
+      }
+    });
+  };
+
   return (
     <Layout>
       <div className="listClientDivButton">
@@ -12,10 +33,28 @@ const ClientForm = () => {
       </div>
       <div className="divForm">
         <form className="form">
-          <input type="text" className="inputForm"></input>
-          <input type="number" className="inputForm"></input>
+          <div className="componentForm">
+            <label>Nome</label>
+            <input
+              type="text"
+              className="inputForm"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="componentForm">
+            <label>Idade</label>
+            <input
+              type="text"
+              className="inputForm"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
           <div className="buttonsForm">
-            <button className="buttonSave">Salvar</button>
+            <button className="buttonSave" onClick={() => saveClient()}>
+              Salvar
+            </button>
             <button className="buttonCancel">Cancelar</button>
           </div>
         </form>
